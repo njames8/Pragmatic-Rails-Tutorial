d3fa941b979f7339bf97a6acfaa6c21ae0656246
Nick James <njames8@outlook.com>
2019-05-04 08:14:59 -0500

    Added users, sessions, and administration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190420132940) do
+ActiveRecord::Schema.define(version: 20190502114251) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -54,6 +54,13 @@ ActiveRecord::Schema.define(version: 20190420132940) do
     t.decimal  "price",       precision: 8, scale: 2
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
